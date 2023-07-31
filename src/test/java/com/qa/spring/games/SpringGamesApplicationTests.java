449d0a4793fa413ba1fc4134401cfecca030df0f
bav-07 <bavaharsan.nagarajah@gmail.com>
2023-07-31 10:21:05 +0100

    first commit

---
+++ src/test/java/com/qa/spring/games/SpringGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.spring.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
